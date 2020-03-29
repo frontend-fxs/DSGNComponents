a7a4feed3effcc7c32570384e92d04e5b550b187
frontend-fxs <oriol@fxstreet.com>
2020-03-29 22:00:44 +0200

    update

---
+++ website/pages/index.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola Mundo</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
