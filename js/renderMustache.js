252ce77c408867dba99ec005f41a7932d985c110
frontend-fxs <oriol@fxstreet.com>
2020-03-31 10:47:09 +0200

    update

---
+++ js/renderMustache.js
@@ -0,0 +1,12 @@
+function renderMustache(datap,templatep,id){
+    const data = fetch('../json/'+datap).then(response => response.json());
+    const template = fetch('../templates/'+templatep).then(response => response.text());
+    Promise.all([data, template])
+        .then(response => {
+            resolvedData = response[0];
+            resolvedTemplate = response[1];
+            Mustache.parse(resolvedTemplate);
+            var output = Mustache.render(resolvedTemplate, resolvedData);
+            return document.querySelector(id).innerHTML = output;
+        }).catch(error => console.log('Unable to get all template data: ', error.message));
+}
