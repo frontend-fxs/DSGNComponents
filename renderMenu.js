2d4aa927d933856630cf5a5c7fd268dc2b4f730b
frontend-fxs <oriol@fxstreet.com>
2020-09-02 16:52:09 +0200

    list upddate

---
+++ renderMenu.js
@@ -0,0 +1,23 @@
+function setActiveItem(active,resolvedData){
+    for(var i = 0; i < resolvedData.entrySections.lenght;i++){
+        if(resolvedData.entrySections[i].name===value){
+            resolvedData.entrySections[i].active=true;
+        }
+    }
+}
+
+function renderMenu(active) {
+  const data = fetch("./menu.json").then((response) => response.json());
+  const template = fetch("./menu.mst").then((response) => response.text());
+  Promise.all([data, template])
+    .then((response) => {
+      resolvedData = response[0];
+      resolvedTemplate = response[1];
+      Mustache.parse(resolvedTemplate);
+      var output = Mustache.render(resolvedTemplate, resolvedData);
+      return (document.querySelector("#fxs_nav_position").innerHTML = output);
+    })
+    .catch((error) =>
+      console.log("Unable to get all template data: ", error.message)
+    );
+}
